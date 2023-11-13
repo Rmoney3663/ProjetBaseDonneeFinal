bab52fb762c9722d16574571d8a57bbaa2eb6d64
Rmoney3663 <75226404+Rmoney3663@users.noreply.github.com>
2023-11-13 14:47:05 -0500

    finished ajouter employe

--- projetFinal/projetFinal/GestionEmploye/AjouterEmploye.cs
+++ projetFinal/projetFinal/GestionEmploye/AjouterEmploye.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -38,6 +39,120 @@ namespace projetFinal.GestionEmploye
 
         private void btnAjouter_Click(object sender, EventArgs e)
         {
+            bool error = false;
+
+            if (CheckPassword(tbPassword.Text))
+            {
+                error = true;
+            }
+
+            if (prenomTextBox.Text.Trim() == "")
+            {
+                errMessage.SetError(prenomTextBox, "Le prenom de l'employé ne peut pas être vide");
+                error = true;
+            }
+
+            if (nomTextBox.Text.Trim() == "")
+            {
+                errMessage.SetError(nomTextBox, "Le nom de l'employé ne peut pas être vide");
+                error = true;
+            }
+
+            if (sexeComboBox.SelectedItem == null)
+            {
+                errMessage.SetError(sexeComboBox, "Le sexe de l'employé ne peut pas être vide");
+                error = true;           
+            } 
+
+            if (noCiviqueTextBox.Text.Trim() == "")
+            {
+                errMessage.SetError(noCiviqueTextBox, "Le numero civique de l'employé ne peut pas être vide");
+                error = true;
+            }
+
+            if (rueTextBox.Text.Trim() == "")
+            {
+                errMessage.SetError(rueTextBox, "Le rue de l'employé ne peut pas être vide");
+                error = true;
+            }
+
+            if (villeTextBox.Text.Trim() == "")
+            {
+                errMessage.SetError(villeTextBox, "La ville de l'employé ne peut pas être vide");
+                error = true;
+            }
+
+            if (idProvinceComboBox.SelectedItem == null)
+            {
+                errMessage.SetError(idProvinceComboBox, "Le province de l'employé ne peut pas être vide");
+                error = true;
+            }
+
+            if (CheckCodePostal(codePostalTextBox.Text))
+            {               
+                error = true;
+            }
+
+            if (CheckPhone(telephoneTextBox.Text))
+            {
+                error = true;
+            }
+
+            if (CheckCell(cellulaireTextBox.Text))
+            {
+                error = true;
+            }
+            if (CheckEmail(courrielTextBox.Text))
+            {
+                error = true;
+            }
+
+            if (noTypeEmployeComboBox.SelectedItem == null)
+            {
+                errMessage.SetError(noTypeEmployeComboBox, "Le type d'employé ne peut pas être vide");
+                error = true;
+            }
+
+            if (error == false)
+            {
+                try
+                {
+                    int largestNumber = dataContext.Employes.Max(employe => employe.No) + 1;
+
+                    Employes newEmploye = new Employes
+                    {
+                        No = largestNumber,
+                        MotDePasse = tbPassword.Text,
+                        Nom = nomTextBox.Text,
+                        Prenom = prenomTextBox.Text,
+                        Sexe = (string)sexeComboBox.SelectedItem,
+                        Age = (int)numAge.Value,
+                        NoCivique = int.Parse(noCiviqueTextBox.Text),
+                        Rue = rueTextBox.Text,
+                        Ville = villeTextBox.Text,
+                        IdProvince = (string)idProvinceComboBox.SelectedValue,
+                        CodePostal = codePostalTextBox.Text,
+                        Telephone = decimal.Parse(telephoneTextBox.Text),
+                        Cellulaire = decimal.Parse(cellulaireTextBox.Text),
+                        Courriel = courrielTextBox.Text,
+                        SalaireHoraire = numSalaire.Value,
+                        NoTypeEmploye = (int)noTypeEmployeComboBox.SelectedValue,
+                        Remarque = remarqueTextBox.Text
+
+                    };
+                    Console.WriteLine(newEmploye);
+
+                    dataContext.Employes.InsertOnSubmit(newEmploye);
+                    dataContext.SubmitChanges();
+                    MessageBox.Show("Employee ajouter", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {                    
+                    MessageBox.Show("Error pour ajouter employé: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+
 
         }
 
@@ -49,7 +164,6 @@ namespace projetFinal.GestionEmploye
 
             var provinces = from p in dataContext.Provinces
                             select p;
-
             idProvinceComboBox.DisplayMember = "Nom"; 
             idProvinceComboBox.ValueMember = "Id";   
             idProvinceComboBox.DataSource = provinces.ToList();
@@ -60,8 +174,177 @@ namespace projetFinal.GestionEmploye
             noTypeEmployeComboBox.DisplayMember = "Description"; 
             noTypeEmployeComboBox.ValueMember = "No";          
             noTypeEmployeComboBox.DataSource = typesEmploye.ToList();
-
-
         }
+
+        private bool CheckPassword(string Password)
+        {
+            if (Password.Trim() == "")
+            {
+                errMessage.SetError(tbPassword, "Le mot de passe ne peut pas être vide");
+                return true;
+            }
+            else
+            {
+                if (Password.Trim().Length < 8)
+                {
+                    errMessage.SetError(tbPassword, "La longeur du mot de passe doit être de minimum 8");
+                    return true;
+                }
+                else
+                {
+                    string verifLettre = @"[A-Za-z]";
+                    if (Regex.IsMatch(Password.Trim(), verifLettre))
+                    {
+                        string verifChiffre = @"\d";
+                        if (Regex.IsMatch(Password.Trim(), verifChiffre))
+                        {
+                            string verifCaractere = @"[^A-Za-z0-9]";
+                            if (Regex.IsMatch(Password.Trim(), verifCaractere))
+                            {
+                                errMessage.SetError(tbPassword, "");
+                                return false;
+                            }
+                            else
+                            {
+                                errMessage.SetError(tbPassword, "Le mot de passe doit contenir au moins un caractère spécial");
+                                return true;
+                            }
+                        }
+                        else
+                        {
+                            errMessage.SetError(tbPassword, "Le mot de passe doit contenir au moins un chiffre");
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        errMessage.SetError(tbPassword, "Le mot de passe doit contenir au moins une lettre");
+                        return true;
+                    }
+                }
+            }            
+        }
+
+        private bool CheckCodePostal(string CodePostal)
+        {
+            if (CodePostal.Trim() == "")
+            {
+                errMessage.SetError(codePostalTextBox, "Le code postal de l'employé ne peut pas être vide");
+                return true;
+            }
+            else
+            {
+                if (CodePostal.Trim().Length == 6)
+                {
+                    Regex canadianPostalCodeRegex = new Regex("^[A-Za-z]\\d[A-Za-z]\\d[A-Za-z]\\d$");
+
+                    if (canadianPostalCodeRegex.IsMatch(CodePostal.Trim()))
+                    {
+                        errMessage.Clear();
+                        return false;
+                    }
+                    else
+                    {
+                        errMessage.SetError(codePostalTextBox, "Le code postal n'est pas au format A#A#A#.");
+                        return true;
+                    }
+                }
+                else
+                {
+                    errMessage.SetError(codePostalTextBox, "La longueur du code postal doit être de 6 caractères.");
+                    return true;
+                }
+
+            }
+        }
+
+        private bool CheckPhone(string NumberPhone)
+        {
+            if (NumberPhone.Trim() == "")
+            {
+                errMessage.SetError(telephoneTextBox, "Le numero de telephone de l'employé ne peut pas être vide");
+                return true;
+            }
+            else
+            {
+                if (NumberPhone.Trim().Length == 10)
+                {
+                    Regex phonenumber = new Regex("^\\d{10}$");
+
+                    if (phonenumber.IsMatch(NumberPhone.Trim()))
+                    {
+                        errMessage.Clear();
+                        return false;
+                    }
+                    else
+                    {
+                        errMessage.SetError(telephoneTextBox, "Le numero de telephone doit contenir seulement des numéros.");
+                        return true;
+                    }
+                }
+                else
+                {
+                    errMessage.SetError(telephoneTextBox, "La longueur du numero de telephone doit être de 10 caractères.");
+                    return true;
+                }
+            }
+        }
+
+        private bool CheckCell(string NumberPhone)
+        {
+            if (NumberPhone.Trim() == "")
+            {
+                errMessage.SetError(cellulaireTextBox, "Le numero de cellulaire de l'employé ne peut pas être vide");
+                return true;
+            }
+            else
+            {
+                if (NumberPhone.Trim().Length == 10)
+                {
+                    Regex phonenumber = new Regex("^\\d{10}$");
+
+                    if (phonenumber.IsMatch(NumberPhone.Trim()))
+                    {
+                        errMessage.Clear();
+                        return false;
+                    }
+                    else
+                    {
+                        errMessage.SetError(cellulaireTextBox, "Le numero de cellulaire doit contenir seulement des numéros.");
+                        return true;
+                    }
+                }
+                else
+                {
+                    errMessage.SetError(cellulaireTextBox, "La longueur du numero de cellulaire doit être de 10 caractères.");
+                    return true;
+                }
+            }
+        }
+
+        private bool CheckEmail(string Email)
+        {
+            if (Email.Trim() == "")
+            {
+                errMessage.SetError(courrielTextBox, "Le courriel de l'employé ne peut pas être vide");
+                return true;
+            }
+            else
+            {               
+                Regex courriel = new Regex("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$");
+
+                if (courriel.IsMatch(Email.Trim()))
+                {
+                    errMessage.Clear();
+                    return false;
+                }
+                else
+                {
+                    errMessage.SetError(courrielTextBox, "Le courriel doit être dans le format d'un courriel valide.");
+                    return true;
+                }
+            }
+        }
+
     }
 }
