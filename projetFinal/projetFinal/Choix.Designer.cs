db02294a7c22865bf398c1999f0f18c206e637b7
Rmoney3663 <75226404+Rmoney3663@users.noreply.github.com>
2023-11-15 11:39:01 -0500

    added all the enable and disable button depending on type

--- projetFinal/projetFinal/Choix.Designer.cs
+++ projetFinal/projetFinal/Choix.Designer.cs
@@ -45,9 +45,10 @@
             // btnSortie
             // 
             this.btnSortie.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSortie.Location = new System.Drawing.Point(15, 715);
+            this.btnSortie.Location = new System.Drawing.Point(11, 581);
+            this.btnSortie.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSortie.Name = "btnSortie";
-            this.btnSortie.Size = new System.Drawing.Size(346, 59);
+            this.btnSortie.Size = new System.Drawing.Size(260, 48);
             this.btnSortie.TabIndex = 23;
             this.btnSortie.Text = "Sortie du système";
             this.btnSortie.UseVisualStyleBackColor = true;
@@ -56,9 +57,10 @@
             // btnDeconnexion
             // 
             this.btnDeconnexion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDeconnexion.Location = new System.Drawing.Point(15, 650);
+            this.btnDeconnexion.Location = new System.Drawing.Point(11, 528);
+            this.btnDeconnexion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDeconnexion.Name = "btnDeconnexion";
-            this.btnDeconnexion.Size = new System.Drawing.Size(346, 59);
+            this.btnDeconnexion.Size = new System.Drawing.Size(260, 48);
             this.btnDeconnexion.TabIndex = 22;
             this.btnDeconnexion.Text = "Déconnexion du système";
             this.btnDeconnexion.UseVisualStyleBackColor = true;
@@ -69,9 +71,10 @@
             this.btnVisualisationStats.BackColor = System.Drawing.SystemColors.Control;
             this.btnVisualisationStats.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnVisualisationStats.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnVisualisationStats.Location = new System.Drawing.Point(15, 585);
+            this.btnVisualisationStats.Location = new System.Drawing.Point(11, 475);
+            this.btnVisualisationStats.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnVisualisationStats.Name = "btnVisualisationStats";
-            this.btnVisualisationStats.Size = new System.Drawing.Size(346, 59);
+            this.btnVisualisationStats.Size = new System.Drawing.Size(260, 48);
             this.btnVisualisationStats.TabIndex = 21;
             this.btnVisualisationStats.Text = "Visualisation des statistiques";
             this.btnVisualisationStats.UseVisualStyleBackColor = false;
@@ -82,9 +85,10 @@
             this.btnVisualisationRapports.BackColor = System.Drawing.SystemColors.Control;
             this.btnVisualisationRapports.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnVisualisationRapports.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnVisualisationRapports.Location = new System.Drawing.Point(15, 520);
+            this.btnVisualisationRapports.Location = new System.Drawing.Point(11, 422);
+            this.btnVisualisationRapports.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnVisualisationRapports.Name = "btnVisualisationRapports";
-            this.btnVisualisationRapports.Size = new System.Drawing.Size(346, 59);
+            this.btnVisualisationRapports.Size = new System.Drawing.Size(260, 48);
             this.btnVisualisationRapports.TabIndex = 20;
             this.btnVisualisationRapports.Text = "Visualisation des rapports";
             this.btnVisualisationRapports.UseVisualStyleBackColor = false;
@@ -95,9 +99,10 @@
             this.btnInscriptionDepense.BackColor = System.Drawing.SystemColors.Control;
             this.btnInscriptionDepense.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnInscriptionDepense.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnInscriptionDepense.Location = new System.Drawing.Point(15, 455);
+            this.btnInscriptionDepense.Location = new System.Drawing.Point(11, 370);
+            this.btnInscriptionDepense.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnInscriptionDepense.Name = "btnInscriptionDepense";
-            this.btnInscriptionDepense.Size = new System.Drawing.Size(346, 59);
+            this.btnInscriptionDepense.Size = new System.Drawing.Size(260, 48);
             this.btnInscriptionDepense.TabIndex = 19;
             this.btnInscriptionDepense.Text = "Inscription d\'une dépense";
             this.btnInscriptionDepense.UseVisualStyleBackColor = false;
@@ -108,9 +113,10 @@
             this.btnInscriptionPartie.BackColor = System.Drawing.SystemColors.Control;
             this.btnInscriptionPartie.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnInscriptionPartie.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnInscriptionPartie.Location = new System.Drawing.Point(15, 390);
+            this.btnInscriptionPartie.Location = new System.Drawing.Point(11, 317);
+            this.btnInscriptionPartie.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnInscriptionPartie.Name = "btnInscriptionPartie";
-            this.btnInscriptionPartie.Size = new System.Drawing.Size(346, 59);
+            this.btnInscriptionPartie.Size = new System.Drawing.Size(260, 48);
             this.btnInscriptionPartie.TabIndex = 18;
             this.btnInscriptionPartie.Text = "Inscription d\'une partie de golf jouée";
             this.btnInscriptionPartie.UseVisualStyleBackColor = false;
@@ -121,9 +127,10 @@
             this.btnModifPrixEtDepenses.BackColor = System.Drawing.SystemColors.Control;
             this.btnModifPrixEtDepenses.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnModifPrixEtDepenses.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnModifPrixEtDepenses.Location = new System.Drawing.Point(15, 325);
+            this.btnModifPrixEtDepenses.Location = new System.Drawing.Point(11, 264);
+            this.btnModifPrixEtDepenses.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnModifPrixEtDepenses.Name = "btnModifPrixEtDepenses";
-            this.btnModifPrixEtDepenses.Size = new System.Drawing.Size(346, 59);
+            this.btnModifPrixEtDepenses.Size = new System.Drawing.Size(260, 48);
             this.btnModifPrixEtDepenses.TabIndex = 17;
             this.btnModifPrixEtDepenses.Text = "Modification des prix et des dépenses d\'abonnement";
             this.btnModifPrixEtDepenses.UseVisualStyleBackColor = false;
@@ -134,9 +141,10 @@
             this.btnMiseAJourAbo.BackColor = System.Drawing.SystemColors.Control;
             this.btnMiseAJourAbo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnMiseAJourAbo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMiseAJourAbo.Location = new System.Drawing.Point(15, 260);
+            this.btnMiseAJourAbo.Location = new System.Drawing.Point(11, 211);
+            this.btnMiseAJourAbo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnMiseAJourAbo.Name = "btnMiseAJourAbo";
-            this.btnMiseAJourAbo.Size = new System.Drawing.Size(346, 59);
+            this.btnMiseAJourAbo.Size = new System.Drawing.Size(260, 48);
             this.btnMiseAJourAbo.TabIndex = 16;
             this.btnMiseAJourAbo.Text = "Mise à jour des abonnés (abonné principal et dépendants)";
             this.btnMiseAJourAbo.UseVisualStyleBackColor = false;
@@ -147,9 +155,10 @@
             this.btnReabonnement.BackColor = System.Drawing.SystemColors.Control;
             this.btnReabonnement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnReabonnement.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnReabonnement.Location = new System.Drawing.Point(15, 195);
+            this.btnReabonnement.Location = new System.Drawing.Point(11, 158);
+            this.btnReabonnement.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnReabonnement.Name = "btnReabonnement";
-            this.btnReabonnement.Size = new System.Drawing.Size(346, 59);
+            this.btnReabonnement.Size = new System.Drawing.Size(260, 48);
             this.btnReabonnement.TabIndex = 15;
             this.btnReabonnement.Text = "Réabonnement";
             this.btnReabonnement.UseVisualStyleBackColor = false;
@@ -160,9 +169,10 @@
             this.btnAbonnement.BackColor = System.Drawing.SystemColors.Control;
             this.btnAbonnement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAbonnement.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAbonnement.Location = new System.Drawing.Point(15, 130);
+            this.btnAbonnement.Location = new System.Drawing.Point(11, 106);
+            this.btnAbonnement.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAbonnement.Name = "btnAbonnement";
-            this.btnAbonnement.Size = new System.Drawing.Size(346, 59);
+            this.btnAbonnement.Size = new System.Drawing.Size(260, 48);
             this.btnAbonnement.TabIndex = 14;
             this.btnAbonnement.Text = "Abonnement";
             this.btnAbonnement.UseVisualStyleBackColor = false;
@@ -172,9 +182,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(132, 9);
+            this.label1.Location = new System.Drawing.Point(99, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(116, 42);
+            this.label1.Size = new System.Drawing.Size(94, 36);
             this.label1.TabIndex = 13;
             this.label1.Text = "Menu";
             // 
@@ -183,9 +194,10 @@
             this.btnGestionEmp.BackColor = System.Drawing.SystemColors.Control;
             this.btnGestionEmp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnGestionEmp.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGestionEmp.Location = new System.Drawing.Point(15, 65);
+            this.btnGestionEmp.Location = new System.Drawing.Point(11, 53);
+            this.btnGestionEmp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnGestionEmp.Name = "btnGestionEmp";
-            this.btnGestionEmp.Size = new System.Drawing.Size(346, 59);
+            this.btnGestionEmp.Size = new System.Drawing.Size(260, 48);
             this.btnGestionEmp.TabIndex = 12;
             this.btnGestionEmp.Text = "Gestion des employés";
             this.btnGestionEmp.UseVisualStyleBackColor = false;
@@ -193,9 +205,9 @@
             // 
             // Choix
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(375, 785);
+            this.ClientSize = new System.Drawing.Size(281, 638);
             this.Controls.Add(this.btnSortie);
             this.Controls.Add(this.btnDeconnexion);
             this.Controls.Add(this.btnVisualisationStats);
@@ -208,8 +220,10 @@
             this.Controls.Add(this.btnAbonnement);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnGestionEmp);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Choix";
             this.Text = "Menu";
+            this.Load += new System.EventHandler(this.Choix_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
